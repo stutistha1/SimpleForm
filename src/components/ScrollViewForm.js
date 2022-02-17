02a6d5e44056e67a803d11203cd6c0ef7484a6af
Stuti Shrestha <shrestha.stuti@gmail.com>
2022-02-17 09:31:22 +0545

    Correcting Form

--- src/components/ScrollViewForm.js
+++ src/components/ScrollViewForm.js
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
-import {Text, TextInput, ScrollView, StyleSheet, Alert} from 'react-native';
+import {
+  Text,
+  TextInput,
+  ScrollView,
+  StyleSheet,
+  Alert,
+  View,
+} from 'react-native';
 
 import {CustomButton, FlatListForm} from '.';
 import {CommonStyles} from '../constants/index';
@@ -13,68 +20,77 @@ const ScrollViewForm = () => {
   const [formData, setFormData] = useState('');
 
   return (
-    <ScrollView style={CommonStyles.flexRoot}>
-      <Text style={STYLE.text}>NEW USER FORM</Text>
+    <View style={CommonStyles.flexRoot}>
+      <ScrollView style={CommonStyles.flexRoot}>
+        <View style={CommonStyles.flexRoot}>
+          <Text style={STYLE.text}>NEW USER FORM</Text>
 
-      <TextInput
-        placeholder="First Name"
-        style={STYLE.input}
-        onChangeText={text => {
-          setFirstName(text);
-        }}
-        value={firstName}
-      />
+          <TextInput
+            placeholder="First Name"
+            style={STYLE.input}
+            onChangeText={text => {
+              setFirstName(text);
+            }}
+            value={firstName}
+          />
 
-      <TextInput
-        placeholder="Last Name"
-        style={STYLE.input}
-        onChangeText={text => {
-          setLastName(text);
-        }}
-        value={lastName}
-      />
+          <TextInput
+            placeholder="Last Name"
+            style={STYLE.input}
+            onChangeText={text => {
+              setLastName(text);
+            }}
+            value={lastName}
+          />
 
-      <TextInput
-        placeholder="Email Address"
-        keyboardType="email-address"
-        style={STYLE.input}
-        onChangeText={text => {
-          setEmail(text);
-        }}
-        value={email}
-      />
+          <TextInput
+            placeholder="Email Address"
+            keyboardType="email-address"
+            style={STYLE.input}
+            onChangeText={text => {
+              setEmail(text);
+            }}
+            value={email}
+          />
 
-      <TextInput
-        placeholder="Age"
-        keyboardType="numeric"
-        style={STYLE.input}
-        onChangeText={text => {
-          setAge(text);
-        }}
-        value={age}
-      />
-      <TextInput placeholder="Phone Number" style={STYLE.input} />
+          <TextInput
+            placeholder="Age"
+            keyboardType="numeric"
+            style={STYLE.input}
+            onChangeText={text => {
+              setAge(text);
+            }}
+            value={age}
+          />
+          <TextInput placeholder="Phone Number" style={STYLE.input} />
 
-      <CustomButton
-        onPress={() => {
-          if (firstName != '' && lastName != '' && email != '' && age != '') {
-            setFormData(arr => [
-              ...arr,
-              {
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                age: age,
-              },
-            ]);
-          } else {
-            Alert.alert('All fields are required');
-          }
-        }}
-        title="Add User"
-      />
+          <CustomButton
+            onPress={() => {
+              if (
+                firstName != '' &&
+                lastName != '' &&
+                email != '' &&
+                age != ''
+              ) {
+                setFormData(arr => [
+                  ...arr,
+                  {
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    age: age,
+                  },
+                ]);
+              } else {
+                Alert.alert('All fields are required');
+              }
+            }}
+            title="Add User"
+          />
+        </View>
+      </ScrollView>
       <FlatListForm Fdata={formData} />
-    </ScrollView>
+    </View>
   );
 };
 
