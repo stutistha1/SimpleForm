2ca06b636855457e2949ccb0de8e66e9afe5ebc4
Stuti Shrestha <shrestha.stuti@gmail.com>
2022-02-17 09:18:39 +0545

    Correcting Form

---
+++ ios/Pods/Target Support Files/Pods-SimpleForm-SimpleFormTests/Pods-SimpleForm-SimpleFormTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleForm_SimpleFormTests : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleForm_SimpleFormTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-SimpleForm/Pods-SimpleForm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleForm : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleForm
+@end
